4e28a588ba28c4ec8b26bf7de72bdd6983e59374
bibenga <{ID}+{username}@users.noreply.github.com>
2025-11-20 11:25:15 +0000

    add some filter

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -13,16 +13,26 @@ module V1
         is_array true
       end
       params do
+        optional :nikname, type: String
+        optional :active, type: Boolean
         optional :_start, type: Integer, values: 0.., default: 0
         optional :_end, type: Integer, values: 1.., default: 10
         optional :_sort, type: String, values: [ "id", "nikname" ], default: "id"
         optional :_order, type: String, values: [ "ASC", "DESC" ], default: "ASC"
       end
       get do
-        authenticate_user!
+        # authenticate_user!
+
+        # pp params
 
         total_count = User.count
         users = User
+        if params[:nikname].present?
+          users = users.where(User.arel_table[:nikname].matches("%#{params[:nikname]}%"), nil, true)
+        end
+        if !params[:active].nil?
+          users = users.where(active: params[:active])
+        end
         if params[:_start].present? && params[:_end].present?
           users = users.offset(params[:_start]).limit(params[:_end] - params[:_start])
         end
@@ -33,11 +43,11 @@ module V1
         present users, with: Entities::User
       end
 
-      desc "Return a user."
-      params do
-        requires :id, type: Integer, desc: "User ID"
-      end
       route_param :id do
+        desc "Return a user."
+        params do
+          requires :id, type: Integer, desc: "User ID"
+        end
         desc "Slected User" do
           entity Entities::User
           failure [ [ 404, "ResourceNotFound", "V1::Entities::Error" ] ]
@@ -47,6 +57,34 @@ module V1
 
           user = User.find(params[:id])
           present user, with: Entities::UserDetail
+        end
+
+        desc "Update a user" do
+          entity Entities::ProductDetail
+          failure [
+            [ 401, "Unauthorized", "V1::Entities::Error" ],
+            [ 403, "PermissionDenied", "V1::Entities::Error" ],
+            [ 404, "ResourceNotFound", "V1::Entities::Error" ],
+            [ 422, "ValidationError", "V1::Entities::Error" ]
+          ]
+        end
+        params do
+          optional :nikname, type: String, allow_blank: false
+          optional :active, type: Boolean
+        end
+        put do
+          authenticate_user!
+
+          user = User.find(params[:id])
+
+          user.nikname = params[:nikname] if params[:nikname]
+          user.active = params[:active] if params[:active]
+
+          if user.save
+            present user, with: Entities::User
+          else
+            error!({ error: user.errors.full_messages }, 422)
+          end
         end
       end
     end
