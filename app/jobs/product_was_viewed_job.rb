90070b5bb4e9982420d888787246aa14ec68ce28
bibenga <{ID}+{username}@users.noreply.github.com>
2025-12-02 10:32:20 +0000

    fix counter

--- app/jobs/product_was_viewed_job.rb
+++ app/jobs/product_was_viewed_job.rb
@@ -3,13 +3,13 @@ class ProductWasViewedJob < ApplicationJob
 
   def perform(product, user)
     ProductCounter.transaction do
-      counter = ProductCounter.lock.find_or_create_by!(product: product.id)
+      counter = ProductCounter.lock.find_or_create_by!(product: product)
       if user.nil?
         logger.info "The product #{product.id} was viewed by anonymous user"
-        counter.increment!(:anonymous_views_count)
+        counter.increment!(:anonymous_views_count, touch: true)
       else
         logger.info "The product #{product.id} was viewed by user #{user&.id}"
-        counter.increment!(:user_views_count)
+        counter.increment!(:user_views_count, touch: true)
       end
     end
   end
