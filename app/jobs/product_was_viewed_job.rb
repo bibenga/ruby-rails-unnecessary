f3569fd455d0248583d6db4deaf7fd9dab929c86
bibenga <{ID}+{username}@users.noreply.github.com>
2025-12-02 10:23:52 +0000

    use increment method

--- app/jobs/product_was_viewed_job.rb
+++ app/jobs/product_was_viewed_job.rb
@@ -2,20 +2,15 @@ class ProductWasViewedJob < ApplicationJob
   queue_as :default
 
   def perform(product, user)
-    if user.nil?
-      logger.info "The product #{product.id} was viewed by anonymous user"
-      ProductCounter.upsert(
-        { product_id: product.id, user_views_count: 0, anonymous_views_count: 1 },
-        unique_by: :product_id,
-        update_only: [ :user_views_count ]
-      )
-    else
-      logger.info "The product #{product.id} was viewed by user #{user&.id}"
-      ProductCounter.upsert(
-        { product_id: product.id, user_views_count: 1, anonymous_views_count: 0 },
-        unique_by: :product_id,
-        update_only: [ :user_views_count ]
-      )
+    ProductCounter.transaction do
+      counter = ProductCounter.lock.find_or_create_by!(product: product.id)
+      if user.nil?
+        logger.info "The product #{product.id} was viewed by anonymous user"
+        counter.increment!(:anonymous_views_count)
+      else
+        logger.info "The product #{product.id} was viewed by user #{user&.id}"
+        counter.increment!(:user_views_count)
+      end
     end
   end
 end
