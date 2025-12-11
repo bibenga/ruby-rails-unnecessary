86f53082631de1dcab2672b8223006ebfca6d991
bibenga <{ID}+{username}@users.noreply.github.com>
2025-12-11 15:07:07 +0000

    fix action text

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -26,3 +26,7 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
+
+// import "controllers"
+import "trix"
+import "@rails/actiontext"
