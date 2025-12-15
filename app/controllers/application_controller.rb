083656bc5a9c72c5bfa67e02558eab1b35b9049d
bibenga <{ID}+{username}@users.noreply.github.com>
2025-12-15 16:02:31 +0000

    remove stale_when_importmap_changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  # stale_when_importmap_changes # vite_rails is used
 
   around_action :switch_locale
 
